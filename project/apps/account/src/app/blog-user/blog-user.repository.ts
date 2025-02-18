ba30455adeb3c4f2d19996a11bc9ba10a1063803
AdonaiJehosua <adonaijehosua@gmail.com>
2025-02-18 21:04:37 +0500

    Создадим репозиторий для BlogUser, укажем, что он может встраиваться в виде зависимости (@Injectable), и зарегистрируем его в модуле в виде экспортируемого параметра

---
+++ project/apps/account/src/app/blog-user/blog-user.repository.ts
@@ -0,0 +1,6 @@
+import {Injectable} from '@nestjs/common';
+import {BaseMemoryRepository} from '@project/core';
+import { BlogUserEntity } from './blog-user.entity';
+
+@Injectable()
+export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {}
