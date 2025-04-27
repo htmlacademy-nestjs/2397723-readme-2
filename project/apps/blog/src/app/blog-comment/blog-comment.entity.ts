6b4d2dc20361a7f73c17dbdeb456c5c62f5ee1bc
AdonaiJehosua <adonaijehosua@gmail.com>
2025-04-27 15:56:25 +0500

    Подготовит модели post и comment

--- project/apps/blog/src/app/blog-comment/blog-comment.entity.ts
+++ project/apps/blog/src/app/blog-comment/blog-comment.entity.ts
@@ -1,9 +1,15 @@
 import {Entity} from '@project/core';
 import {Comment} from '@project/types';
+import {Record} from 'prisma/prisma-client/runtime/library';
 
 export class BlogCommentEntity implements Comment, Entity<string> {
   public id: string;
   public text: string;
   public author: string;
   public creationDate: Date;
+
+  public toObject(): Record<string, unknown> {
+    return {}
+  }
 }
+
