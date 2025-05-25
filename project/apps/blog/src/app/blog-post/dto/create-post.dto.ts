faa54786367391a6859d432b2b3ce854802ac736
AdonaiJehosua <adonaijehosua@gmail.com>
2025-05-25 12:13:40 +0500

    Поправит создание нового поста, подключит валидацию

--- project/apps/blog/src/app/blog-post/dto/create-post.dto.ts
+++ project/apps/blog/src/app/blog-post/dto/create-post.dto.ts
@@ -1,45 +1,52 @@
 import {PostType} from '@project/types';
-import {IsDate, IsNotEmpty, IsString} from 'class-validator';
+import {IsNotEmpty, IsOptional, IsString} from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
   public authorId: string;
 
+  @IsString()
   @IsNotEmpty()
   public type: PostType;
 
   @IsString()
+  @IsOptional()
   public title?: string;
 
   @IsString()
+  @IsOptional()
   public youtubeLink?: string;
 
   @IsString()
+  @IsOptional()
   public preview?: string;
 
   @IsString()
+  @IsOptional()
   public textPostText?: string;
 
   @IsString()
+  @IsOptional()
   public quotePostText?: string;
 
   @IsString()
+  @IsOptional()
   public quoteAuthor?: string;
 
   @IsString()
+  @IsOptional()
   public photo?: string;
 
   @IsString()
+  @IsOptional()
   public link?: string;
 
   @IsString()
+  @IsOptional()
   public description?: string;
 
   @IsString()
+  @IsOptional()
   public tags?: string;
-
-  @IsString()
-  @IsDate()
-  public creationDate: Date;
 }
--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import {Logger, ValidationPipe} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
@@ -16,6 +16,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
