a7c3da1c474c0b73e7ed4e907e7d6ba1fad525f6
AdonaiJehosua <adonaijehosua@gmail.com>
2025-05-31 14:29:15 +0500

    Создаст конфиги lkz mongo для работы с модулем file-manager, добавит типы

--- project/shared/types/src/index.ts
+++ project/shared/types/src/index.ts
@@ -4,3 +4,5 @@ export * from './lib/post/post.interface';
 export * from './lib/post/post-type.enum';
 export * from './lib/post/reposted-post.interface';
 export * from './lib/post/comment.interface';
+export * from './lib/file/file.interface'
+export * from './lib/file/stored-file.interface'
