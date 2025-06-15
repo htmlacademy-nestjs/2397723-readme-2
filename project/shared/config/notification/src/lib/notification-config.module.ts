108184170a0da32d66794e8789a15f4921715c4b
AdonaiJehosua <adonaijehosua@gmail.com>
2025-06-15 15:53:49 +0500

    Установит пакеты для реализации подписки, подключит логирование в призме, подготовит работу с RabbitMQ

---
+++ project/shared/config/notification/src/lib/notification-config.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import notificationConfig from './notification.config';
+
+const ENV_FILE_PATH = 'apps/notification/notification.env';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [notificationConfig],
+      envFilePath: ENV_FILE_PATH,
+    }),
+  ]
+})
+export class NotificationConfigModule { }
